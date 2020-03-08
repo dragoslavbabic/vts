80ee782e65d4f5d3d8a0bedcdda0d25577bd0b2c
Dragoslav Babić <dragance@uns.ac.rs>
2020-03-08 12:05:19 +0100

    VTS - ORIGINAL

---
+++ js/navbar1.js
@@ -0,0 +1,115 @@
+document.getElementById("navjs").innerHTML =
+    '<div class="navbar-header">' +
+    '<button type="button" class="navbar-toggle my-button" data-toggle="collapse" data-target="#myNavbar">' +
+    '<i class="glyphicon glyphicon-list">' +
+    '<!--a>MENI</a-->' +
+    '</i>' +
+    '</button>' +
+    '</div>' +
+    '<div class="col-lg-10 col-md-10 col-sm-10 col-xs-8 collapse navbar-collapse" id="myNavbar">' +
+    '<ul class="nav navbar-nav">' +
+    '<li><a href="index.html">Насловна</a></li>' +
+    '<li class="dropdown">' +
+    '<a class="dropdown-toggle" data-toggle="dropdown" href="#">О школи <span class="caret"></span></a>' +
+    '<div class="dropdown-menu multi-column">' +
+    '<div class="container-fluid">' +
+    '<div class="row-fluid">' +
+    '<div class="span4">' +
+    '<ul class="dropdown-menu">' +
+    '<li><a href="#">Закон о високом образовању</a></li>' +
+    '<li><a href="#">Самовредновање</a></li>' +
+    '</ul>' +
+    '</div>' +
+    '<div class="span4">' +
+    '<ul class="dropdown-menu">' +
+    '<li><a href="#">Општа акта школе</a></li>' +
+    '<li><a href="#">Кадар</a></li>' +
+    '</ul>' +
+    '</div>' +
+    '<div class="span4">' +
+    '<ul class="dropdown-menu">' +
+    '<li><a href="#">Jaвне набавке</a></li>' +
+    '<li><a href="#">Акредитација</a></li>' +
+    '</ul>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</li>' +
+    '<li class="dropdown">' +
+    '<a class="dropdown-toggle" data-toggle="dropdown" href="#">Студије<span class="caret"></span></a>' +
+    '<div class="dropdown-menu multi-column">' +
+    '<div class="container-fluid">' +
+    '<div class="row-fluid">' +
+    '<div class="span4">' +
+    '<ul class="dropdown-menu">' +
+    '<li><a href="#">Закон о високом образовању</a></li>' +
+    '<li><a href="#">Самовредновање</a></li>' +
+    '</ul>' +
+    '</div>' +
+    '<div class="span4">' +
+    '<ul class="dropdown-menu">' +
+    '<li><a href="#">Општа акта школе</a></li>' +
+    '<li><a href="#">Кадар</a></li>' +
+    '</ul>' +
+    '</div>' +
+    '<div class="span4">' +
+    '<ul class="dropdown-menu">' +
+    '<li><a href="#">Jaвне набавке</a></li>' +
+    '<li><a href="#">Акредитација</a></li>' +
+    '</ul>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</div>' +
+    '</li>' +
+    '<li class="dropdown">' +
+    '<a class="dropdown-toggle" data-toggle="dropdown" href="#">Упис<span class="caret"></span></a>' +
+    '</li>' +
+    '<li class="dropdown">' +
+    '<a class="dropdown-toggle" data-toggle="dropdown" href="#">Сарадња и развој<span class="caret"></span></a>' +
+    '</li>' +
+    '<li class="dropdown">' +
+    '<a class="dropdown-toggle" data-toggle="dropdown" href="#">Вести<span class="caret"></span></a>' +
+    '</li>' +
+    '</ul>' +
+    '<!--moj vts nalog-->' +
+    '</div>' +
+    '<ul class="col-lg-2col-md-2 col-sm-2 col-xs-4 nav navbar-nav navbar-right my-login">' +
+    '<li class="dropdown active">' +
+    '<a href="#" class="dropdown-toggle" data-toggle="dropdown">Мој ВТШ<b class="caret"></b></a>' +
+    '<ul class="dropdown-menu login-dropdown" style="padding: 15px;min-width: 250px;">' +
+    '<li>' +
+    '<div class="row">' +
+    '<div class="col-md-12">' +
+    '<form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">' +
+    '<div class="form-group">' +
+    '<label class="sr-only" for="emailInput">Email address</label>' +
+    '<input type="email" class="form-control" id="emailInput" placeholder="Email address" required />' +
+    '</div>' +
+    '<div class="form-group">' +
+    '<label class="sr-only" for="passwordInput">Password</label>' +
+    '<input type="password" class="form-control" id="passwordInput" placeholder="Password" required />' +
+    '</div>' +
+    '<div class="checkbox">' +
+    '<label>' +
+    '<input type="checkbox" /> Remember me' +
+    '</label>' +
+    '</div>' +
+    '<div class="form-group">' +
+    '<button type="submit" class="btn btn-success btn-block">' +
+    'Sign in' +
+    '</button>' +
+    '</div>' +
+    '</form>' +
+    '</div>' +
+    '</div>' +
+    '</li>' +
+    '<li class="divider"></li>' +
+    '<li>' +
+    '<input class="btn btn-primary btn-block" type="button" id="sign-in-google" value="Sign In with Google" />' +
+    '<input class="btn btn-primary btn-block" type="button" id="sign-in-twitter" value="Sign In with Twitter" />' +
+    '</li>' +
+    '</ul>' +
+    '</li>' +
+    '</ul>';
